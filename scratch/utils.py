520ef2cbc03914fd30c6596f21b2f42cf4847c4b
Andrea Wynn <andreawynn@Andreas-Laptop.local>
2024-10-31 17:19:44 -0400

    Adding new layerskip implementation

--- scratch/utils.py
+++ scratch/utils.py
@@ -6,6 +6,167 @@ import requests
 import matplotlib.pyplot as plt
 import os
 import numpy as np
+import transformers
+
+
+# Based on the implementation from: https://github.com/facebookresearch/LayerSkip/blob/main/self_speculation/llama_model_utils.py#L213
+def get_next_logits(
+    model: transformers.LlamaForCausalLM,
+    input_ids: torch.Tensor,
+    exit_layer: int,
+):
+    device = input_ids.device
+    batch_size, seq_length = input_ids.shape
+    seq_length_with_past = seq_length
+    past_key_values_length = 0
+    past_key_values = None
+
+    position_ids = torch.arange(
+        past_key_values_length,
+        seq_length + past_key_values_length,
+        dtype=torch.long,
+        device=device,
+    )
+    position_ids = position_ids.unsqueeze(0).view(-1, seq_length)
+    
+    # Constructing an attention mask of the same shape as the input IDs. 
+    attention_mask = input_ids.new_ones( 
+        (batch_size, seq_length_with_past),
+        dtype=torch.bool,
+    )
+    inputs_embeds = model.model.embed_tokens(input_ids)
+    attention_mask = _prepare_decoder_attention_mask(
+        model,
+        attention_mask,
+        (batch_size, seq_length),
+        inputs_embeds,
+        past_key_values_length,
+    )
+
+    hidden_states = inputs_embeds
+    # Propagate through the decoder layers up until the early exit layer
+    for decoder_layer in model.model.layers[:exit_layer]:
+        if past_key_values is not None:
+            hidden_states, past_key_values = decoder_layer(
+                hidden_states,
+                attention_mask=attention_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_values,
+                output_attentions=False,
+                use_cache=True,
+                padding_mask=None,)
+        else:
+            hidden_states, past_key_values = decoder_layer(
+            hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            output_attentions=False,
+            use_cache=True,
+            padding_mask=None,)
+
+    # past_key_values = past_key_values.to_legacy_cache()
+
+    hidden_states = model.model.norm(hidden_states)
+    logits = model.lm_head(hidden_states)
+    
+    return logits
+
+
+def top_k_top_p_filtering(
+    logits: torch.FloatTensor,
+    top_k: int = 0,
+    top_p: float = 1.0,
+    filter_value: float = -float("Inf"),
+    min_tokens_to_keep: int = 1,
+) -> torch.FloatTensor:
+    if top_k > 0:
+        logits = transformers.generation.logits_process.TopKLogitsWarper(top_k=top_k, filter_value=filter_value, min_tokens_to_keep=min_tokens_to_keep)(
+            None, logits
+        )
+
+    if 0 <= top_p <= 1.0:
+        logits = transformers.generation.logits_process.TopPLogitsWarper(top_p=top_p, filter_value=filter_value, min_tokens_to_keep=min_tokens_to_keep)(
+            None, logits
+        )
+
+    return logits
+
+# Decoding via sampling, not just argmax
+def decode_next_token(
+    logits: torch.Tensor,
+    sample: bool = False,
+    temperature: float = 0.7,
+    top_k: int = 50,
+    top_p: float = 0.95,
+) -> torch.Tensor:
+    if not sample:
+        next_token = logits.argmax(dim=-1)
+        return next_token, None
+    else:
+        logits.squeeze_(dim=0)
+        filtered_logits = top_k_top_p_filtering(logits / temperature, top_k=top_k, top_p=top_p)
+        probabilities = torch.nn.functional.softmax(filtered_logits, dim=-1)
+        next_token = torch.multinomial(probabilities, num_samples=1)
+        next_token.transpose_(1, 0)
+        return next_token, probabilities
+
+
+# Copied from transformers.models.bart.modeling_bart._make_causal_mask
+def _make_causal_mask(
+    input_ids_shape: torch.Size, dtype: torch.dtype, device: torch.device, past_key_values_length: int = 0
+):
+    """
+    Make causal mask used for bi-directional self-attention.
+    """
+    bsz, tgt_len = input_ids_shape
+    mask = torch.full((tgt_len, tgt_len), torch.finfo(dtype).min, device=device)
+    mask_cond = torch.arange(mask.size(-1), device=device)
+    mask.masked_fill_(mask_cond < (mask_cond + 1).view(mask.size(-1), 1), 0)
+    mask = mask.to(dtype)
+
+    if past_key_values_length > 0:
+        mask = torch.cat([torch.zeros(tgt_len, past_key_values_length, dtype=dtype, device=device), mask], dim=-1)
+    return mask[None, None, :, :].expand(bsz, 1, tgt_len, tgt_len + past_key_values_length)
+
+# Copied from transformers.models.bart.modeling_bart.BartDecoder._prepare_decoder_attention_mask
+def _prepare_decoder_attention_mask(model, attention_mask, input_shape, inputs_embeds, past_key_values_length):
+    # create causal mask
+    # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+    combined_attention_mask = None
+    if input_shape[-1] > 1:
+        combined_attention_mask = _make_causal_mask(
+            input_shape,
+            inputs_embeds.dtype,
+            device=inputs_embeds.device,
+            past_key_values_length=past_key_values_length,
+        )
+
+    if attention_mask is not None:
+        # [bsz, seq_len] -> [bsz, 1, tgt_seq_len, src_seq_len]
+        expanded_attn_mask = _expand_mask(attention_mask, inputs_embeds.dtype, tgt_len=input_shape[-1]).to(
+            inputs_embeds.device
+        )
+        combined_attention_mask = (
+            expanded_attn_mask if combined_attention_mask is None else expanded_attn_mask + combined_attention_mask
+        )
+
+    return combined_attention_mask
+
+# Copied from transformers.models.bart.modeling_bart._expand_mask
+def _expand_mask(mask: torch.Tensor, dtype: torch.dtype, tgt_len = None):
+    """
+    Expands attention_mask from `[bsz, seq_len]` to `[bsz, 1, tgt_seq_len, src_seq_len]`.
+    """
+    bsz, src_len = mask.size()
+    tgt_len = tgt_len if tgt_len is not None else src_len
+
+    expanded_mask = mask[:, None, None, :].expand(bsz, 1, tgt_len, src_len).to(dtype)
+
+    inverted_mask = 1.0 - expanded_mask
+
+    return inverted_mask.masked_fill(inverted_mask.to(torch.bool), torch.finfo(dtype).min)
+
+
 
 
 def get_intermediate_embeddings(model, inputs, max_length):
